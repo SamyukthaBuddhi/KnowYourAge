fab223f7e13058a4e776e8818bbb2d84a9d1338a
sam050895 <samyukthabuddhi05@gmail.com>
2022-03-19 14:07:22 +0530

    First commit

---
+++ sc.js
@@ -0,0 +1,21 @@
+function add_number() {
+
+    var first_number = parseInt(document.getElementById("text").value);
+    var a =parseInt(2);
+    var b= parseInt(5);
+    var c =parseInt(50);
+    var d = parseInt(1771);
+    // var e = parseInt(1770);
+    var second_number = parseInt(document.getElementById("text1").value);
+    var result = first_number * a;
+    var f =result + b;
+    var g= f * c;
+    
+    
+        document.getElementById("text2").value=d+g-second_number;
+  
+}
+
+
+
+
